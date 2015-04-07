cb8160a524a944645a284a95f50cf9eecf57a3f3
Dmytro Voznenko <dvoznenko@backbase.com>
2015-04-07 11:04:58 +0300

    core-2 : added Spring Boot application structure

---
+++ src/main/java/org/perspectiveJuniors/YAQM/Runner/Application.java
@@ -0,0 +1,12 @@
+package org.perspectiveJuniors.YAQM.runner;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+    }
+}
