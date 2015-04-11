ee357f5d0687f8fc9d996570edb931901c7a405d
Dmytro Voznenko <dvoznenko@backbase.com>
2015-04-11 18:08:13 +0300

    core-3 : configured MySQL as a datasource

--- src/main/java/org/perspectiveJuniors/YAQM/Runner/Application.java
+++ src/main/java/org/perspectiveJuniors/YAQM/Runner/Application.java
@@ -1,5 +1,6 @@
 package org.perspectiveJuniors.YAQM.runner;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
@@ -8,5 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
     }
 }
