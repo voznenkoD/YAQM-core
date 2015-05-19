d4078ff6d4b94acbef9f78a0c0bc6acd04b97aca
Dmytro Voznenko <dvoznenko@backbase.com>
2015-05-19 21:31:37 +0300

    Refactor

--- src/main/java/org/perspectiveJuniors/YAQM/entity/AbstractPersonalData.java
+++ src/main/java/org/perspectiveJuniors/YAQM/entity/AbstractPersonalData.java
@@ -1,8 +1,45 @@
 package org.perspectiveJuniors.YAQM.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
 /**
  * Created by Всеволод on 11.04.2015.
  */
+@Entity
+@Table(name="PERSONAL_DATA")
 public abstract class AbstractPersonalData {
+    @Column(name="NAME")
+    private String name;
+    @Column(name="SURNAME")
+    private String surname;
+    @Column(name="LOGIN")
+    private String login;
+    @OneToOne
+    @PrimaryKeyJoinColumn
+    private AbstractUser absUser;
+
+    public AbstractPersonalData(){};
+
+    public AbstractPersonalData(String name, String surname, String login) {
+        this.name = name;
+        this.surname = surname;
+        this.login = login;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getLogin() {
+        return login;
+    }
 
 }
--- src/main/java/org/perspectiveJuniors/YAQM/entity/impl/PersonalData.java
+++ src/main/java/org/perspectiveJuniors/YAQM/entity/impl/PersonalData.java
@@ -8,36 +8,7 @@ import javax.persistence.*;
 /**
  * Created by Всеволод on 11.04.2015.
  */
-@Entity
-@Table(name="PERSONAL_DATA")
+
 public class PersonalData extends AbstractPersonalData{
-    @Column(name="NAME")
-    private String name;
-    @Column(name="SURNAME")
-    private String surname;
-    @Column(name="LOGIN")
-    private String login;
-    @OneToOne
-    @PrimaryKeyJoinColumn
-    private AbstractUser absUser;
 
-    public PersonalData(){};
-
-    public PersonalData(String name, String surname, String login) {
-        this.name = name;
-        this.surname = surname;
-        this.login = login;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public String getLogin() {
-        return login;
-    }
 }
