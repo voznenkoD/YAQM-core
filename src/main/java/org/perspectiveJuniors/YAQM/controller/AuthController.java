a9a3326897044e3c61aba08205eaedf1d89793fc
Dmytro Voznenko <dvoznenko@backbase.com>
2015-07-04 13:04:26 +0300

    add access denied support

--- src/main/java/org/perspectiveJuniors/YAQM/controller/AuthController.java
+++ src/main/java/org/perspectiveJuniors/YAQM/controller/AuthController.java
@@ -2,8 +2,12 @@ package org.perspectiveJuniors.YAQM.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.LockedException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -48,6 +52,23 @@ public class AuthController {
         }
 
         return error;
+    }
+
+    @RequestMapping(value = "/403", method = RequestMethod.GET)
+    public ModelAndView accesssDenied() {
+
+        ModelAndView model = new ModelAndView();
+
+        // check if user is login
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (!(auth instanceof AnonymousAuthenticationToken)) {
+            UserDetails userDetail = (UserDetails) auth.getPrincipal();
+            model.addObject("username", userDetail.getUsername());
+        }
+
+        model.setViewName("403");
+        return model;
+
     }
 
 }
